032c9456b3ec9ce1a4a34d7598f5fc0498eb1718
DWE-CLOUD <85799123+DWE-CLOUD@users.noreply.github.com>
2024-10-16 00:26:55 +0530

    Feat: Fixed App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import React, { useState } from 'react';
 import BrandingCard from '../src/components/cards/components/BrandingCard';
 import WebDevelopmentCard from '../src/components/cards/components/WebDevCard';
 import DigitalMarketingCard from '../src/components/cards/components/DigitalMarketingCard';
@@ -10,21 +9,15 @@ import Testimonials from '../src/components/cards/components/Testimonials';
 import SignupPage from '../src/components/Login/SignupPage';
 import LoginPage from '../src/components/Login/LoginPage';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { motion } from 'framer-motion';
 
 function App() {
-    const [darkMode, setDarkMode] = useState(false);
-
-    const toggleDarkMode = () => {
-        setDarkMode((prevMode) => !prevMode);
-    };
 
     return (
-        <div className={`App ${darkMode ? 'dark' : ''}`}>
+        <div className="App">
             <Router>
                 <Routes>
-                    <Route path="/signup" element={<SignupPage />} />
-                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="/signup" element={<SignupPage/>}/>
+                    <Route path="/login" element={<LoginPage/>}/>
                     <Route
                         path="*"
                         element={
@@ -41,32 +34,32 @@ function App() {
                 <div className="container mx-auto max-w-screen-xl">
                     <h2
                         className="text-4xl mb-8 text-center"
-                        style={{ fontFamily: 'Roboto', fontSize: '40px', fontWeight: 'bold', color: '#FFFFFF' }}
+                        style={{fontFamily: 'Roboto', fontSize: '40px', fontWeight: 'bold', color: '#FFFFFF'}}
                     >
                         We Offer
                     </h2>
                     <p
                         className="text-center mb-12"
-                        style={{ fontFamily: 'Roboto', fontSize: '16px', color: '#9E9E9E' }}
+                        style={{fontFamily: 'Roboto', fontSize: '16px', color: '#9E9E9E'}}
                     >
                         Risus commodo id odio turpis pharetra elementum. Pulvinar porta porta
                         feugiat
-                        <br /> scelerisque in elit. Morbi rhoncus, tellus, eros consequat magna
+                        <br/> scelerisque in elit. Morbi rhoncus, tellus, eros consequat magna
                         semper orci a
-                        <br /> tincidunt.
+                        <br/> tincidunt.
                     </p>
                     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-                        <BrandingCard />
-                        <WebDevelopmentCard />
-                        <DigitalMarketingCard />
-                        <MobileAppCard />
-                        <SeoCard />
-                        <UserTestingCard />
+                        <BrandingCard/>
+                        <WebDevelopmentCard/>
+                        <DigitalMarketingCard/>
+                        <MobileAppCard/>
+                        <SeoCard/>
+                        <UserTestingCard/>
                     </div>
                 </div>
             </div>
 
-            <Testimonials />
+            <Testimonials/>
         </div>
     );
 }
