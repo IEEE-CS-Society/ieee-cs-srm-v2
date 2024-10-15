98c0504195e3ba12de800baf2a62da3b8383aff1
MadBoy <73926361+madboy482@users.noreply.github.com>
2024-10-15 22:45:54 +0530

    Added offer page and students' testimonials

---
+++ client/offer/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'custom-black': '#1a1616',
+      },
+    },
+  },
+  plugins: [],
+}
