c197d9196f3a6380509e952ce0b6a930e862ce64
DWOSCLOUD <85799123+DWE-CLOUD@users.noreply.github.com>
2024-10-20 15:04:37 +0530

    Update main.py

--- server/main.py
+++ server/main.py
@@ -24,7 +24,7 @@ app = FastAPI(docs_url=None, redoc_url=None)
 #run command : python -m uvicorn main2:app --reload
 
 origins = [
-    "http://ieee-cs-srm-v2.vercel.app","https://ieee-cs-srm-v2.vercel.app","http://localhost:3001","https://127.0.0.1:3001","http://localhost:8000","https://127.0.0.1:8000"
+    "http://ieee-cs-srm-v2.vercel.app","https://ieee-cs-srm-v2.vercel.app","http://ieee-cs-srm-v2.vercel.app/login","https://ieee-cs-srm-v2.vercel.app/login","http://ieee-cs-srm-v2.vercel.app/signup","https://ieee-cs-srm-v2.vercel.app/signup","http://localhost:3001","https://127.0.0.1:3001","http://localhost:8000","https://127.0.0.1:8000"
 ]
 app.add_middleware(
     CORSMiddleware,
